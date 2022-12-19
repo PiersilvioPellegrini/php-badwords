90bef30b928a16829ac052c0cd10e94713c419ff
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-12-19 18:41:50 +0100

    problem with localhost

--- data.php
+++ data.php
@@ -8,11 +8,13 @@ $message = $_GET["message"];
 echo "PAROLA DA CENSURARE:" . $badword . "<br><br>";
 echo "MESSAGGIO:" . $message . "<br><br>";
 
+echo strlen($message);
+
 // funzione che sostituisce la parola "PIER" con il valore desiderato
 // valore1 : parola da sotituire
 // valore2: parola che sostituisce l'originale
 // //valore3: punto in cui cercare e sotituire parola ricercata 
 
-echo str_replace("pier", "xxxx", $message);
+echo str_replace("$badword", "xxxx", $message);
 
 ?>
