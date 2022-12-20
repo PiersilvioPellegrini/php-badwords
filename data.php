b85ff11e05cecea1f259ce3ae578435f3b6a1e15
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-12-20 09:54:07 +0100

    COMPLETED

--- data.php
+++ data.php
@@ -4,13 +4,14 @@
 $badword = $_GET["badword"];
 $message = $_GET["message"];
 
+var_dump($_GET);
 // visualizzo i dati 
 echo "PAROLA DA CENSURARE:" . $badword . "<br><br>";
 echo "MESSAGGIO:" . $message . "<br><br>";
 
 echo strlen($message);
 
-// funzione che sostituisce la parola "PIER" con il valore desiderato
+// funzione che sostituisce la parola "inserita" con il valore desiderato
 // valore1 : parola da sotituire
 // valore2: parola che sostituisce l'originale
 // //valore3: punto in cui cercare e sotituire parola ricercata 
